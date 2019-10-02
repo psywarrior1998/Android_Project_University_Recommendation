a1299f1cfa9165026428f2642f9aca022604db69
Sanyam Sharma <sanyamsharma252@gmail.com>
2019-10-02 14:18:39 +0530

    Menu Finalised with options value file v 0.1.0

--- app/src/main/java/com/example/androidproject/searchpage.java
+++ app/src/main/java/com/example/androidproject/searchpage.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class searchpage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchpage);
-    }
-
-    public void showmenu(View view) {
-
+        Spinner countries = (Spinner) findViewById(R.id.Countries);
+        ArrayAdapter<CharSequence> country = ArrayAdapter.createFromResource(this,
+                R.array.country, android.R.layout.simple_spinner_item);
+        country.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countries.setAdapter(country);
+        Spinner courses = (Spinner) findViewById(R.id.Course);
+        ArrayAdapter<CharSequence> course = ArrayAdapter.createFromResource(this,
+                R.array.course, android.R.layout.simple_spinner_item);
+        course.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        courses.setAdapter(course);
     }
 }
