43a284d189ab4afa4727e1ecef857a59f7699335
Sanyam Sharma <sanyamsharma252@gmail.com>
2019-10-02 16:56:04 +0530

    Firebasae Authentication added v0.1.1

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,26 +1,63 @@
 package com.example.androidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "activity_main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void login(View view) {
-        Intent intent = new Intent(MainActivity.this, detailspage.class);
-        startActivity(intent);
+        EditText emailid = findViewById(R.id.usertext);
+        EditText pass = findViewById(R.id.passtext);
+        String email = emailid.getText().toString();
+        String password = pass.getText().toString();
+        signIn(email,password);
     }
 
     public void signup(View view) {
         Intent intent = new Intent(MainActivity.this, signup.class);
         startActivity(intent);
+    }
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "login success");
+                            Toast.makeText(MainActivity.this,
+                                    "Authenticated\n ---welcome---",
+                                    Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(MainActivity.this, detailspage.class);
+                            startActivity(intent);
+                        } else {
+                            Log.e(TAG, "Login fail", task.getException());
+                            Toast.makeText(MainActivity.this,
+                                    "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/androidproject/signup.java
+++ app/src/main/java/com/example/androidproject/signup.java
@@ -1,14 +1,58 @@
 package com.example.androidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "activity_signup";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void Signup(View view) {
+        EditText emailid = (EditText) findViewById(R.id.usertext);
+        EditText pass = (EditText) findViewById(R.id.passtext);
+        String email = emailid.getText().toString();
+        String password = pass.getText().toString();
+
+        registerAccount(email,password);
+    }
+    private void registerAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "account created");
+                            Toast.makeText(signup.this,
+                                    "Registered.",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(signup.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Log.d(TAG, "register account failed", task.getException());
+                            Toast.makeText(signup.this,
+                                    "account registration failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
